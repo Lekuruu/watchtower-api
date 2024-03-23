70f3b2c6516d79082e803ff18db24858b48bbd5a
Lekuru <contact@lekuru.xyz>
2024-03-23 20:42:50 +0100

    Add postgres configuration

--- config.py
+++ config.py
@@ -10,6 +10,12 @@ REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 REDIS_DB = os.getenv("REDIS_DB", 0)
 
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+POSTGRES_DB = POSTGRES_USER
+
 API_HOST = os.getenv("API_HOST", "0.0.0.0")
 API_PORT = os.getenv("API_PORT", 8080)
 
