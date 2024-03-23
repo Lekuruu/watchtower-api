651f5f5fdb5226f48ce93e2698d9e255a0e36b44
Lekuru <contact@lekuru.xyz>
2024-03-23 17:30:30 +0100

    Add initial fastapi project structure

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+
+from fastapi import (
+    HTTPException,
+    Response,
+    Request,
+    FastAPI
+)
+
+from . import routes
+
+import logging
+import uvicorn
+
+logging.basicConfig(
+    format='[%(asctime)s] - <%(name)s> %(levelname)s: %(message)s',
+    level=logging.INFO
+)
+
+api = FastAPI(
+    title='API',
+    version='0.0.1',
+    redoc_url=None
+)
+
+api.include_router(routes.router)
+
+def run():
+    uvicorn.run(
+        api,
+        host='0.0.0.0', # TODO
+        port=8080,      # TODO
+        log_config=None
+    )
