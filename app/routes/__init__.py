91b67ac7a32f3342a81b1cd7e4e7dc115c60ffb9
Lekuru <contact@lekuru.xyz>
2024-03-23 19:10:46 +0100

    Add placeholder scores router

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,8 +3,10 @@ from fastapi.responses import HTMLResponse
 from fastapi import APIRouter
 
 from . import updates
+from . import scores
 from . import stats
 
 router = APIRouter()
 router.include_router(updates.router)
+router.include_router(scores.router)
 router.include_router(stats.router)
